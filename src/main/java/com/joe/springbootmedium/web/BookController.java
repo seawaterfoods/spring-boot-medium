fdf07d2e19811c0472b52ea909b7a051f9fef4f9
seawaterfood <99390124@ss25.mcu.edu.tw>
2020-06-09 14:15:08 +0800

    於Controller異常錯誤處理。

--- src/main/java/com/joe/springbootmedium/web/BookController.java
+++ src/main/java/com/joe/springbootmedium/web/BookController.java
@@ -3,17 +3,24 @@ package com.joe.springbootmedium.web;
 import com.joe.springbootmedium.domain.Book;
 import com.joe.springbootmedium.exception.BookNotFoundException;
 import com.joe.springbootmedium.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import sun.security.provider.certpath.OCSPResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
+
+    private final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     private BookService bookService;
@@ -25,5 +32,23 @@ public class BookController {
         Book book =bookService.getBookById(id);
         model.addAttribute("book",book);
         return "book";
+    }
+
+
+//    於Controller錯誤處理，只能處理這個Controller
+    @ExceptionHandler({Exception.class})
+    public ModelAndView handleException(HttpServletRequest request,Exception e) throws Exception {
+
+        logger.error("Request URL : {} , Exception : {}", request.getRequestURL(),e.getMessage());
+//      若有已設定異常狀態碼之異常則直接拋出
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null){
+            throw e;
+        }
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.addObject("url",request.getRequestURL());
+        modelAndView.addObject("exception",e);
+        modelAndView.setViewName("error/error");
+
+        return modelAndView;
     }
 }
